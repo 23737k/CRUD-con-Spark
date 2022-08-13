dde37d686498e6335d0fefdea4a5ce782f94877e
kenti <016mki69@gmail.com>
2022-08-13 11:30:08 -0300

    Feat: envio de mails

--- src/main/java/ar/edu/utn/dds/libros/AppLibros.java
+++ src/main/java/ar/edu/utn/dds/libros/AppLibros.java
@@ -1,14 +1,17 @@
 package ar.edu.utn.dds.libros;
+import javax.mail.MessagingException;
 import spark.Spark;
 
 public class AppLibros {
         public void init(){
     }
-    public void start(){
+    public void start() throws MessagingException {
         Spark.port(getHerokuAssignedPort());
+        MailSender mailSender=new MailSender(); //ATENCION: NO FUNCIONA. HAT QUE PONER LA PASS EN MAILSENDER Y UN MAIL VALIDO EN SENDMAIL
+        mailSender.sendMail("unMail","Hola, usted tiene una nueva notificacion");
         Router.init();
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MessagingException {
         AppLibros app = new AppLibros();
         app.init();
         app.start();
